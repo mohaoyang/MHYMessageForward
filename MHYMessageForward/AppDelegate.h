19bbe18da407886f6c4e5bb4ac8d52eca83fd36e
mhy <qhmumhy@163.com>
2019-02-11 15:32:41 +0800

    Initial Commit

---
+++ MHYMessageForward/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MHYMessageForward
+//
+//  Created by jshtmhy on 2019/2/11.
+//  Copyright © 2019年 jshtmhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MHYMessageForward/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MHYMessageForward
+//
+//  Created by jshtmhy on 2019/2/11.
+//  Copyright © 2019年 jshtmhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
